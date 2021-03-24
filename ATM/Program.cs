bc373bf1ffda0dbad859a4c56646deb753b843f3
Vavyo <vavyo2121@gmail.com>
2021-03-23 20:22:50 -0500

    User input pathing

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -4,6 +4,7 @@ namespace ATM
 {
     class Program
     {
+        static public decimal Balance;
         static void Main(string[] args)
         {
             UserInterface();
@@ -11,7 +12,41 @@ namespace ATM
 
         private static void UserInterface()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Welcome to the Bank of Gaming!");
+            bool active = true;
+            while (active)
+            {
+                string userInput = Console.ReadLine();
+                string[] splitInput = userInput.Split(':');
+                string command = splitInput[0];
+                decimal amount;
+                try
+                {
+                    amount = Convert.ToDecimal(splitInput[1]);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    command = null;
+                }
+                switch (command)
+                {
+                    case "balance":
+                        break;
+                    case "withdraw":
+                        break;
+                    case "deposit":
+                        break;
+                    case "help":
+                        Console.WriteLine("balance - Display your current balance.");
+                        Console.WriteLine("withdraw:[amount] - Withdraws [amount] from your account.");
+                        Console.WriteLine("deposit:[amount] - Deposit [amount] into your account.");
+                        Console.WriteLine("help - Displays available commands");
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, I don't understand... try 'help' for a list of commands!");
+                        break;
+                }
+            }
         }
     }
 }
